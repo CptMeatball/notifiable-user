7adedefa054d6af8e718cc7407cd3a1567981b26
Antonie Hogewoning <antonie.hogewoning@gmail.com>
2018-03-01 23:34:52 +0100

    Clean up on aisle 5

--- Plugin.php
+++ Plugin.php
@@ -1,6 +1,5 @@
 <?php namespace CptMeatball\NotifiableUser;
 
-use Backend;
 use RainLab\User\Models\User;
 use System\Classes\PluginBase;
 
@@ -9,7 +8,7 @@ use System\Classes\PluginBase;
  */
 class Plugin extends PluginBase
 {
-    public $require = ['RainLab.User'];
+    public $require = ['Rainlab.User'];
 
     /**
      * Returns information about this plugin.
@@ -27,16 +26,6 @@ class Plugin extends PluginBase
     }
 
     /**
-     * Register method, called when the plugin is first registered.
-     *
-     * @return void
-     */
-    public function register()
-    {
-
-    }
-
-    /**
      * Boot method, called right before the request route.
      *
      * @return array
@@ -46,56 +35,5 @@ class Plugin extends PluginBase
         User::extend(function($model) {
             $model->implement[] = 'CptMeatball.NotifiableUser.Behaviors.Notifiable';
         });
-    }
-
-    /**
-     * Registers any front-end components implemented in this plugin.
-     *
-     * @return array
-     */
-    public function registerComponents()
-    {
-        return []; // Remove this line to activate
-
-        return [
-            'CptMeatball\NotifiableUser\Components\MyComponent' => 'myComponent',
-        ];
-    }
-
-    /**
-     * Registers any back-end permissions used by this plugin.
-     *
-     * @return array
-     */
-    public function registerPermissions()
-    {
-        return []; // Remove this line to activate
-
-        return [
-            'cptmeatball.notifiableuser.some_permission' => [
-                'tab' => 'NotifiableUser',
-                'label' => 'Some permission'
-            ],
-        ];
-    }
-
-    /**
-     * Registers back-end navigation items for this plugin.
-     *
-     * @return array
-     */
-    public function registerNavigation()
-    {
-        return []; // Remove this line to activate
-
-        return [
-            'notifiableuser' => [
-                'label'       => 'NotifiableUser',
-                'url'         => Backend::url('cptmeatball/notifiableuser/mycontroller'),
-                'icon'        => 'icon-leaf',
-                'permissions' => ['cptmeatball.notifiableuser.*'],
-                'order'       => 500,
-            ],
-        ];
     }
 }
